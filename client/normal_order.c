9d5e996b5bdeeb982e59b4f19439f28087d7cb3f
daou_taejun <xowns1016@daou.co.kr>
2025-04-01 09:18:54 +0900

    client 주문 후 대기

--- client/normal_order.c
+++ client/normal_order.c
@@ -87,6 +87,13 @@ void run_normal_order() {
         threads[i] = CreateThread(NULL, 0, client_thread, &client_ids[i], 0, NULL);
     }
 
+    // 엔터 키 대기
+    printf("엔터 키를 누르면 다음으로 진행합니다...");
+    fflush(stdout); // 출력이 버퍼에 남지 않도록
+    getchar();      // 첫 번째 엔터 처리용 (만약 이전 입력 버퍼에 엔터가 남아있을 경우)
+    getchar();
+
+
     WaitForMultipleObjects(MULTI_CLIENT, threads, TRUE, INFINITE);
     printf("모든 클라이언트 종료\n");
     return 0;
